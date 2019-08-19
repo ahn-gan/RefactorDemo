25b2a8f257c3927e25593e3a8f514cc70df5be1e
Akin <akin.gan@oocl.com>
2019-08-19 20:29:00 +0800

    [feat: add the test should_return_38_when_given_item_with_general_name_and_sellIn_is_0 and should_return_40_when_given_item_with_Sulfuras_Hand_of_Ragnaros_name_and_sellIn_is_f1]
    [feat: change the test name should_return_50_when_given_item_with_Aged_Brie_name_and_sellIn_is_f1 to should_return_0_when_given_item_with_Aged_Brie_name_and_sellIn_is_f1]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -140,7 +140,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_50_when_given_item_with_Aged_Brie_name_and_sellIn_is_f1() {
+    public void should_return_0_when_given_item_with_Aged_Brie_name_and_sellIn_is_f1() {
         // given
         Item[] items = {new Item("Aged Brie", 10, -1)};
         GildedRose gildedRose = new GildedRose(items);
@@ -149,6 +149,30 @@ public class GildedRoseTest {
 
         Assert.assertEquals(0, items[0].getQuality());
         Assert.assertEquals(9, items[0].getSellIn());
+    }
+
+    @Test
+    public void should_return_38_when_given_item_with_general_name_and_sellIn_is_0() {
+        // given
+        Item[] items = {new Item("test name", 0, 40)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        Assert.assertEquals(38, items[0].getQuality());
+        Assert.assertEquals(-1, items[0].getSellIn());
+    }
+
+    @Test
+    public void should_return_40_when_given_item_with_Sulfuras_Hand_of_Ragnaros_name_and_sellIn_is_f1() {
+        // given
+        Item[] items = {new Item("Sulfuras, Hand of Ragnaros", -1, 40)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        Assert.assertEquals(40, items[0].getQuality());
+        Assert.assertEquals(-1, items[0].getSellIn());
     }
 
 }
